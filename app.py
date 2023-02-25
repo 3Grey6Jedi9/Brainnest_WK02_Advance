57b2fb0aa0abb098ff42bdaa1ee714a0f602b4d4
Darth Vader <danieltarancon@gmail.com>
2023-02-25 19:54:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import google.auth
 import google.auth.transport.requests
 from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
-from key import api_key_file
+from key import useful_data
+import base64
 
 
 
@@ -21,11 +22,28 @@ msg['To'] = 'intersum369@gmail.com'
 msg.set_content('This is a test email sent from Python.')
 
 
+# Converting the EmailMessage to a format compatible with the Gmail API
+mime_msg = msg.as_string().encode("utf-8")
+b64_msg = base64.urlsafe_b64encode(mime_msg).decode("utf-8")
+raw_msg = {"raw": b64_msg}
+
+
 # Connecting with the server
-def authenticate_gmail_api(api_key_file):
+def authenticate_gmail_api(useful_data):
     # Set up the OAuth 2.0 flow
-    flow = InstalledAppFlow.from_client_secrets_file(
-        api_key_file,
+    flow = InstalledAppFlow.from_client_config(
+        {
+            "web": {
+                "client_id": useful_data['client_id'],
+                "project_id": useful_data['project_id'],
+                "auth_uri": useful_data['auth_uri'],
+                "token_uri": useful_data['token_uri'],
+                "auth_provider_x509_cert_url": useful_data['auth_provider_x509_cert_url'],
+                "client_secret": useful_data['client_secret'],
+                "redirect_uris": useful_data['redirect_uris'],
+                "javascript_origins": [],
+            }
+        },
         scopes=['https://www.googleapis.com/auth/gmail.send']
     )
     credentials = flow.run_local_server(port=0)
@@ -35,7 +53,8 @@ def authenticate_gmail_api(api_key_file):
     return service
 
 
-service = authenticate_gmail_api(api_key_file)
+service = authenticate_gmail_api(useful_data)
+
 
 
 
@@ -49,7 +68,7 @@ def send_email(service, message):
         message = None
     return message
 
-send_email(service, msg)
+send_email(service, raw_msg)
 
 
 
