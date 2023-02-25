e9f97c54eb8e277f7538b88784dc8a0a952fff3f
Darth Vader <danieltarancon@gmail.com>
2023-02-25 21:42:40 +0100

    Update key.py

--- key.py
+++ key.py
@@ -1,5 +1,5 @@
 import json
-import getpass
+
 
 with open('/Users/danielmulatarancon/Desktop/Documents/HACKING TIME/Brainnest /Week 02/Advance Tasks/OAuth/client_secret_google.json') as f:
     secret_data = json.load(f)
@@ -7,5 +7,5 @@ with open('/Users/danielmulatarancon/Desktop/Documents/HACKING TIME/Brainnest /W
 
 useful_data = secret_data['installed']
 
-
+password = ''
 
